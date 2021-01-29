78fd22c1b12461aa528c6b4c63362d210bc15f9d
KlaudioK <kkucelin@securworks.com>
2021-01-28 19:58:14 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Space Cades!'));
+app.get('/', (req, res) => res.send('Hello Space Cadets!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
